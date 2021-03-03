2a394098779f6e782df59cf420173df3f9676703
miss jody <missjodyrussell@gmail.com>
2021-03-03 15:41:17 -0700

    updated NoMatch page, centered text and added additional components

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import Footer from "../components/Footer"
+import Portfolio from '../components/Portfolio';
+import resumeData from '../resumeData';
 
 class NoMatch extends Component {
     render() {
     return (
         <div>
+        <section id="wrongTurn">
             <h1> Sorry, that page does not exist </h1>
+        </section>
+        <Portfolio resumeData={resumeData}/>
+        <Footer resumeData={resumeData} />
         </div>
     )
 }
