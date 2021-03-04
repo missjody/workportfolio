8cf59c6eb40a637bab4a6beab1f9d73ca602aaa3
miss jody <missjodyrussell@gmail.com>
2021-03-03 20:58:40 -0700

    stumped on how to pass the information to Projects

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ import Projects from "./pages/Projects";
 import Header from './components/Header';
 import resumeData from './resumeData';
  
+// note from 3/3: when changing the projects route like this
+// can we additionally drill down to grab just the project based on
+// what is being passed from current press 
+
+
 class App extends Component {
     render() {
         return (
@@ -22,9 +27,12 @@ class App extends Component {
               <Route exact path={["/", "/home"]}>
                 <Home />
               </Route>
-              <Route path="/projects">
-                <Projects />
-              </Route>
+              <Route 
+                exact path="/projects/:id"
+                render={props => 
+                  <Projects project={this.state.project}
+                  />
+                }/>
               <Route>
                 <NoMatch />
               </Route>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import resumeData from "../resumeData";
 import Footer from "../components/Footer"
 import Portfolio from '../components/Portfolio';
+import About from '../components/About';
 
 // this page needs to be passed the location in the array that the selection 
 // is at so that it populates the page with that project
 
 // flesh out once info is passing to page correctly
 
-export default class ProjectSoloPage extends Component {
-  render() {
+const Projects = (project) => {
+
       
     // let resumeData = this.props.resumeData;
 
-    console.log("resumeData ", resumeData)
+    console.log("project data: ", project )
 
     return (
       <div>
-        <section>
+        <section id="projectsSection">
 
           <h1> Test </h1>
+          <h3>
+          Project Title 
 
-          {/* Project Image */}
-          {/* Project Title */}
-          {/* Project Link 1  */}
-          {/* Project Link 2 if true */}
-          {/* Project Description */}
+          Project Image
+          Project Description
+          Project Link 1 Project Link 2 if true
+          </h3>
+
         </section>
-          <Portfolio resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
         <Footer resumeData={resumeData} />
         </div>
     )
   }
-}
+
+  export default Projects;
