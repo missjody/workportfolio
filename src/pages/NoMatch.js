10a658e8da2db5a4ffb56acf139186ae8ee251b3
Jody Russell <missjodyrussell@gmail.com>
2021-04-14 09:37:44 -0600

    UI for projects at 80%

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -5,16 +5,16 @@ import resumeData from '../resumeData';
 
 class NoMatch extends Component {
     render() {
-    return (
-        <div>
-        <section id="wrongTurn">
-            <h1 id="wrongTurnH"> Sorry, that page does not exist </h1>
-        </section>
-        <Portfolio resumeData={resumeData}/>
-        <Footer resumeData={resumeData} />
-        </div>
-    )
-}
+        return (
+            <div>
+                <section id="wrongTurn">
+                    <h1 id="wrongTurnH"> Sorry, that page does not exist </h1>
+                </section>
+                {/* <Portfolio resumeData={resumeData}/>
+        <Footer resumeData={resumeData} /> */}
+            </div>
+        )
+    }
 }
 
 export default NoMatch;
